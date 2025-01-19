e2571a8bd1b6fe0621e5fc3511c5c9974471eeab
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-01-19 11:05:48 +0100

    Réalisations des exercices  1 à 4

--- script.js
+++ script.js
@@ -49,11 +49,19 @@ console.log("___________________________________________________________________
 
 //Exercice 4 : Les Fonctions
 console.log("Exercice 4 : Fonction de calcul")
+console.log("")
 console.log("Addition")
 //1-Fonction addition  qui prend deux nombres
+console.log("function addition(nombre1, nombre2)")
+console.log("retourne nombre1 + nombre2")
 function addition(nombre1, nombre2){
     return nombre1 + nombre2; //retourne leur somme
 }
-
+console.log("")
+console.log("J'ai déclaré la variable somme afin d'additionner les deux deux nombres")
+console.log("let somme = addition(10, 15)")
 let somme = addition(10 , 15); //j'ai déclaré la variable somme qui additionne deux nombres
+console.log("")
+console.log("Affiche le résultat de la somme")
 console.log(somme);//Affiche le résultat de la somme des deux nombres
+console.log("_________________________________________________________________________________________")
