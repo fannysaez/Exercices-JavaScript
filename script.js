943df4f1bf9fea5473d2b67ba777f615da058e8c
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-01-19 06:59:47 +0100

    Réalisation de l'exercice 1 : Les Bases des variables

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+//Exercice 1 : Les bases des variables
+
 console.log("Exercice 1 : Les bases des variables")
-let prenom = "Fanny";
-let nom = "Saez";
+let prenom = "Fanny"; //J'ai déclaré la variable "prenom"
+let nom = "Saez";//J'ai déclaré la variable "nom"
 console.log("")
+//Affichage sur la console de ma variable prénom qui contient mon prénom
 console.log("1. J'ai déclaré une variable prénom qui contient mon prénom")
 console.log("Fanny")
 console.log("")
+//Affichage sur la console de ma variable nom qui contien mon nom
 console.log("2. J'ai déclaré une variable nom qui contient mon nom")
 console.log("Saez")
 console.log("")
+//Affichage d'une phrase sur ma console "Bonjour, je m'appelle [nom] [prenom]"
 console.log("3. J'affiche une phrase dans la console : Bonjour, je m'appelle [nom] [prénom].")
 console.log("Bonjour" + "," + " je m'appelle" + " " + nom + " " + prenom + ".")
+
+//Exercice 2 : Conditions simples avec input
